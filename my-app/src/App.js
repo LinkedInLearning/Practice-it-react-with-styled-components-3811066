709cec5fe2f3bbe07240faf7895dd73aa858b357
juliaedyck <juliaedyck@gmail.com>
2025-08-14 12:50:20 +0200

    04_02

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,10 +3,15 @@ import axios from "axios";
 import ProductDetails from "./components/examples/ProductDetails";
 import Parent from "./components/examples/Parent";
 import ProductList from "./components/examples/ProductList";
+import GlobalStyles from "./components/examples/GlobalStyles";
 
 const App = () => {
+
   return (
     <div>
+      <GlobalStyles />
+      <h1>Global Styles Example</h1>
+
       <ProductList />
     </div>
   );
