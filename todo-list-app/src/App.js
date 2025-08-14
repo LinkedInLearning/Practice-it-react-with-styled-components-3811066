2b8864593429d4d851fcabd55ca7c450a3bef155
juliaedyck <juliaedyck@gmail.com>
2025-08-14 12:43:47 +0200

    03_06

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -1,41 +1,15 @@
 import React from "react";
+import TodoList from "./TodoList";
 
-const Todo = ({ title }) => {
-  return <li>{title}</li>;
-};
-
-const TodoList = ({ todos }) => {
+const App = () => {
   const todos = ["Buy groceries", "Finish homework", "Go for a run"];
 
-  // Write your answer here, and then test your code.
-  // Your job is to implement the FindLargest() component.
   return (
-    <ul>
-      <Todo title={todos[0]} />
-      <Todo title={todos[1]} />
-      <Todo title={todos[2]} />
-    </ul>
+    <div>
+      <h1>My Todo List</h1>
+      <TodoList todos={todos} />
+    </div>
   );
 };
 
-function App() {
-  return (
-    <div>
-      <TodoList />
-    </div>
-  );
-}
-
-/* Ignore and do not change the code below */
-/**
- * Preview function
- * This function renders the `App` component and is used for visual rendering
- * during the challenge. Candidates do not need to modify this.
- */
-export function Preview() {
-  return <App />;
-}
-/* Ignore and do not change the code above */
-
-// Export the main App component
 export default App;
--- todo-list-app/src/TodoList.js
+++ todo-list-app/src/TodoList.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Todo from "./Todo";
 
-const TodoList = ({todos}) => {
+const TodoList = ({ todos }) => {
   return (
     <ul>
-      <Todo title={todos[0]} />
-      <Todo title={todos[1]} />
-      <Todo title={todos[2]} />
+      {todos.map((todo, index) => (
+        <Todo key={index} title={todo} />
+      ))}
     </ul>
   );
 };
