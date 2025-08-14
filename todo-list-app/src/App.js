6f6d386f2171bfe142c3f555d2e98c03e50e3890
juliaedyck <juliaedyck@gmail.com>
2025-08-14 10:35:02 +0200

    01_02

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -1,10 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
-import TodoList from "./TodoList";
+import React from "react";
 
-function App() {
+const Todo = ({ title }) => {
+  return <li>{title}</li>;
+};
+
+const TodoList = ({ todos }) => {
   const todos = ["Buy groceries", "Finish homework", "Go for a run"];
 
+  // Write your answer here, and then test your code.
+  // Your job is to implement the FindLargest() component.
+  return (
+    <ul>
+      <Todo title={todos[0]} />
+      <Todo title={todos[1]} />
+      <Todo title={todos[2]} />
+    </ul>
+  );
+};
+
+function App() {
   return (
     <div>
       <TodoList />
@@ -12,4 +26,16 @@ function App() {
   );
 }
 
+/* Ignore and do not change the code below */
+/**
+ * Preview function
+ * This function renders the `App` component and is used for visual rendering
+ * during the challenge. Candidates do not need to modify this.
+ */
+export function Preview() {
+  return <App />;
+}
+/* Ignore and do not change the code above */
+
+// Export the main App component
 export default App;
