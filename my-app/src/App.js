310b66d05325ab0df6d5b098aa93993d1ad97a7d
juliaedyck <juliaedyck@gmail.com>
2025-08-14 11:09:21 +0200

    02_01

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,26 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  return <div></div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => setData(response.data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>My React App</h1>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
