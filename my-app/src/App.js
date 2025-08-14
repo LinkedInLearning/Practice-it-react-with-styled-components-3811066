a732d45a78056b6fe2aa3eaad95926a7055bded3
juliaedyck <juliaedyck@gmail.com>
2025-08-14 12:18:54 +0200

    03_02

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ProductDetails from "./components/examples/ProductDetails";
+import Parent from "./components/examples/Parent";
 
 const App = () => {
   return (
     <div>
       <ProductDetails />
+      <Parent />
     </div>
   );
 };
